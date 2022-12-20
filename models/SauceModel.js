2d8fe16422ecd7cf41ddb30dcecaca72fea45bfc
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-12-20 11:25:04 +0100

    update SaucesModel

--- models/SauceModel.js
+++ models/SauceModel.js
@@ -4,6 +4,25 @@ const SauceSchema = new mongoose.Schema({
     name: {
         type: String, 
         required: true
+    },
+    manufacturer: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    mainPepper: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    heat: {
+        type: Number
     }
 })
 
